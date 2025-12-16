433ed3235fbc287997d03f4ccc413b1cac23b0db
M Junaid Fida <junaidfida03@gmail.com>
2025-12-16 19:17:30 +0500

    feat: add navbar and hero bg

---
+++ app/(home)/hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='font-ChakraPetch  bg-cover h-screen bg-HeroBg'>
+<div> 
+  <div>
+<div> </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/src/components/navbar'
+import Hero from './(home)/hero'
+
+const page = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+  )
+}
+
+export default page
